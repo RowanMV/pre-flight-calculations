54942b7bc57450e1603923c97a944bbaf0272b9a
sarahrhemadayal <80999949+sarahrhemadayal@users.noreply.github.com>
2024-05-23 02:05:41 +0530

    updated wandb

--- wandb.py
+++ wandb.py
@@ -4,30 +4,27 @@ def weightconv(pounds: float) -> float:
     '''
     Takes weight in pounds as input and converts it to kilogrammes'''
     return pounds*0.453592
-    raise NotImplementedError
 
 def lengthconv(inches: float) -> float:
     '''
     Takes length in inches as input and converts it to metres'''
     return inches*0.0254
-    raise NotImplementedError
 
 def weightinp(raw: str) -> float:
     '''
-    Takes as input the string inputted by the user, detects the units and converts it floato an floateger in kilogrammes'''
-    weightdata = raw.split(1)
+    Takes as input the string inputted by the user, detects the units and converts it into a float in kilogrammes'''
+    weightdata = raw.split(' ', 1)
     weighttypes = {"lbs", "pounds", "lb"}
     if weightdata[1] in weighttypes:
-        weightconv(weightdata[0])
-    raise NotImplementedError
+        return weightconv(float(weightdata[0]))
 
 def lengthinp(raw: str) -> float:
     '''
-    Takes as input the string inputted by the user, detects the units and converts it floato an floateger in metres'''
-    lengthdata = raw.split(1)
+    Takes as input the string inputted by the user, detects the units and converts it into a float in metres'''
+    lengthdata = raw.split(' ', 1)
     lengthtype = {"inch", "inches", "in"}
     if lengthdata[1] in lengthtype:
-        lengthconv(lengthdata[0])
+        return lengthconv(float(lengthdata[0]))
 
 def check(raw: str) -> bool:
     '''
