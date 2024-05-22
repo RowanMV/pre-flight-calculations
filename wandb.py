b0ecad48560094080270ba0f69e731dec36cd610
RowanMV <rvedangi24@gmail.com>
2024-05-22 20:35:04 +0100

    Added structure

--- wandb.py
+++ wandb.py
@@ -1,1 +1,11 @@
+# Currently need 2 unit converters, one from pounds to kg and one from inches to metre
 
+def weightconv(pounds: int) -> int:
+    '''
+    Takes weight in pounds as input and converts it to kilogrammes'''
+    raise NotImplementedError
+
+def lengthconv(inches: int) -> int:
+    '''
+    Takes length in inches as input and converts it to metres'''
+    raise NotImplementedError
