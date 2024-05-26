f13b8780c4e1270403b6a68feec8f3ba5d624940
RowanMV <rvedangi24@gmail.com>
2024-05-26 21:03:17 +0100

    Started weather processing code

--- weather.py
+++ weather.py
@@ -1,1 +1,13 @@
-
+raw = input('Input the weather forecast: ')
+if 'TAF' in raw:
+    # Start processing a standard TAF message
+    pass
+elif 'METAR' in raw:
+    # Start processing a standar METAR message
+    data = raw.split()
+    ad = data[1]
+    day = data[2][:2]
+    time = data[2][2:]
+    wind = data[3]
+    vis = data[4]
+    pass
