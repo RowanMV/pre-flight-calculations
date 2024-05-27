ad518da211d7a563e36b9d6689fc18f21ce42260
RowanMV <rvedangi24@gmail.com>
2024-05-27 18:49:52 +0100

    fixed spacebar semantics

--- weather.py
+++ weather.py
@@ -53,7 +53,7 @@ elif 'METAR' in raw:
     time = data[2][2:]
     wind = data[3]
     vis = data[4]
-    output = f'METAR observation for aerodrome {ad} taken at the {day} day of the month at {time} UTC. Winds are {wind} and visibility is {vis}. '
+    output = f'METAR observation for aerodrome {ad} taken at the {day} day of the month at {time} UTC. Winds are {wind} and visibility is {vis}.'
 
     # Iterate through each statement in the dataset and check if there is any information about cloud layer
     for statement in data:
@@ -62,7 +62,7 @@ elif 'METAR' in raw:
                 level = int(data[-3:])
                 height = str(int * 100)
                 cldtype = cloud[data[:4]]
-                output += f'{cldtype} at {height} feet. '
+                output += f' {cldtype} at {height} feet.'
                 if 'TCU' in statement:
                     # Additional processing for towering cumulus
                     raise NotImplementedError
