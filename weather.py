cdbb7d07024d5d1851dab7e80e023353d774cc49
RowanMV <rvedangi24@gmail.com>
2024-05-28 13:51:04 +0100

    removed dictionaries and moved them to another file

--- weather.py
+++ weather.py
@@ -1,60 +1,6 @@
+from wthrvrbls import cloud, wthr, digits_to_words
+
 raw = input('Input the weather forecast: ')
-
-# Decoding dictionaries
-cloud = {
-    'FEW': 'Few clouds',
-    'SCT': 'Scattered layer',
-    'BKN': 'Broken layer',
-    'OVC': 'Overcast layer',
-    'NSC': 'No significant cloud'
-}
-wthr = {
-    'BC': 'patches',
-    'BL': 'blowing',
-    'BR': 'mist',
-    'DR': 'drifting',
-    'BC': 'duststorm',
-    'DU': 'dust',
-    'DZ': 'drizzle',
-    'FC': 'funnel cloud',
-    'FG': 'fog',
-    'FU': 'smoke',
-    'FZ': 'freezing',
-    'GR': 'hail',
-    'GS': 'small hail',
-    'HZ': 'haze',
-    'IC': 'ice crystals',
-    'MI': 'shallow',
-    'PL': 'ice pellets',
-    'PO': 'dust devils',
-    'PR': 'banks',
-    'RA': 'rain',
-    'SA': 'sand',
-    'SH': 'showers',
-    'SG': 'snow grain',
-    'SN': 'snow',
-    'SQ': 'squalls',
-    'SS': 'sandstorm',
-    'TS': 'thunderstorm',
-    'VA': 'volcanic ash',
-    'VC': 'in vicinity',
-    'UP': 'unidentified precipitation'
-}
-
-#Dic mapping digits to words
-digits_to_words = {
-    '0': 'zero',
-    '1': 'one',
-    '2': 'two',
-    '3': 'three',
-    '4': 'four',
-    '5': 'five',
-    '6': 'six',
-    '7': 'seven',
-    '8': 'eight',
-    '9': 'nine'
-}
-
 
 if 'TAF' in raw:
     # Start processing a standard TAF message
